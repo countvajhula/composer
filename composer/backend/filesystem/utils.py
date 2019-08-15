0f31fe3f5434a84381bded152159211070c5cd22
Siddhartha <sid@countvajhula.com>
2019-08-15 12:49:46 -0700

    fix contain mutation decorator

--- composer/backend/filesystem/utils.py
+++ composer/backend/filesystem/utils.py
@@ -25,9 +25,14 @@ def contain_file_mutation(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         new_args = [copy_file(arg) if isinstance(arg, StringIO) else arg for arg in args]
-        new_kwargs = dict([(k, copy_file(v)) if isinstance(v, StringIO) else (k, v) for k, v in kwargs.items()])
-        return fn(*new_args, **new_kwargs)
-    return fn
+        new_kwargs = {k: (copy_file(v) if isinstance(v, StringIO) else v) for k, v in kwargs.items()}
+        result = fn(*new_args, **new_kwargs)
+        try:
+            new_result = [copy_file(r) if isinstance(r, StringIO) else r for r in result]
+        except TypeError:
+            new_result = copy_file(result) if isinstance(result, StringIO) else result
+        return new_result
+    return wrapper
 
 
 def quarter_for_month(month):
