1cbb3071a6e2337bf1c60f8f9c9803341364c75a
Siddhartha <sid@countvajhula.com>
2019-08-23 12:24:27 -0700

    an extra safeguard to prevent extra newlines in templates

--- composer/backend/filesystem/templates/base.py
+++ composer/backend/filesystem/templates/base.py
@@ -1,6 +1,21 @@
 import abc
+import re
+
+from ..utils import is_task
 
 ABC = abc.ABCMeta("ABC", (object,), {})  # compatible with Python 2 *and* 3
+
+
+def _tidy_item(item):
+    # TODO: need a test for a case where there are extra newlines in
+    # template components, to prove that this helps by eliminating them
+    extra_newlines = re.compile('\n{2}$')
+    if item == '\n':
+        item = ''
+    elif extra_newlines.search(item):
+        item = item.rstrip('\n')
+        item += '\n'
+    return item
 
 
 class Template(ABC):
@@ -27,24 +42,26 @@ class Template(ABC):
     def build(self):
         template = ""
         if self.title:
+            self.title = _tidy_item(self.title)
             template = self.title
             template += "\n"
         if self.entry:
+            self.entry = _tidy_item(self.entry)
             template += self.entry
             template += "\n"
         template += "CHECKPOINTS:\n"
         for line in self.checkpointsfile:
-            if line[:3] == "[ ]":
+            if is_task(line):
                 template += line
         template += "\n"
         template += "AGENDA:\n"
         if self.agenda:
+            self.agenda = _tidy_item(self.agenda)
             template += self.agenda
-            template += "\n"
         template += "\n"
         template += self.periodicname
         for line in self.periodicfile:
-            if line[:3] == "[ ]":
+            if is_task(line):
                 template += line
         template += "\n"
         template += "NOTES:\n\n\n"
