03c874ed241eb8056721917fd1d9434e641b5d17
Siddhartha <sid@countvajhula.com>
2019-11-14 17:35:58 -0800

    Bump version: 3.0.0 → 3.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup_requirements = ['pytest-runner']
 
 setup(
     name='composer',
-    version='3.0.0',
+    version='3.1.0',
     description='A life tracking, organizing, and planning framework.',
     author='Siddhartha Kasivajhula',
     author_email='sid@countvajhula.com',
